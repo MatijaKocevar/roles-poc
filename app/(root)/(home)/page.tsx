8452243daacfa45c962db0de4fa4630d31bff8e1
Matija Kočevar <matija.kocev@gmail.com>
2025-05-01 04:51:14 +0200

    Update page.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,7 @@
 export default async function HomePage() {
     return (
         <div className="flex w-full h-full justify-center items-center text-4xl font-extrabold">
-            POC - FLEX ROLE MANAGEMENT
+            POC - ROLE MANAGEMENT
         </div>
     );
 }
